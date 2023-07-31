acf9fb272c353438edb404aff48a2e8e731b9b69
Wiphawanyaun <gitwiphawan@gmail.com>
2023-08-01 03:18:13 +0700

    slide-sucess

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 
 const splide = new Splide('.splide',{
-      type: 'loop', //slide, loop, fade
+      type: 'slide', //slide, loop, fade
       // padding: { y: 10 }, //css values
-      gap: '5rem', //1 rem default
+      gap: '4.5rem', //1 rem default
       // rewind: true,
       speed: 500, //transition in milliseconds
       // fixedWidth: '10vw',
       // // width: '100vw',
-      perPage: 2,
+      perPage: 1,
       // start: 3,
       autoWidth:true,
       perMove: 1,
       // autoplay: true,
       // interval: 3000,
       // arrows: false,
-      // pagination: false,
+      // drag   : false,
+      pagination: false,
       // pauseOnHover: true,
       // wheel: true,
 });splide.mount();
