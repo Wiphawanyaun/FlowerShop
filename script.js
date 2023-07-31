21ed953c655d128c6645b7e62f45b630542013b9
Wiphawanyaun <gitwiphawan@gmail.com>
2023-08-01 00:58:11 +0700

    fix-slide

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 
-new Splide('#box-catalog').mount();
-document.addEventListener('click', function () {
-      new Splide('#box-catalog', {
-            fixedWidth: 400,
-            perPage:3,
-            drag:false,
-
-      }).mount();
-});
+const splide = new Splide('.splide',{
+      type: 'loop', //slide, loop, fade
+      // padding: { y: 10 }, //css values
+      gap: '5rem', //1 rem default
+      // rewind: true,
+      speed: 500, //transition in milliseconds
+      // fixedWidth: '10vw',
+      // // width: '100vw',
+      perPage: 2,
+      // start: 3,
+      autoWidth:true,
+      perMove: 1,
+      // autoplay: true,
+      // interval: 3000,
+      // arrows: false,
+      // pagination: false,
+      // pauseOnHover: true,
+      // wheel: true,
+});splide.mount();
 
 
 
