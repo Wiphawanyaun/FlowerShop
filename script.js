b21014141df23dd404ae9e5625ccf8492e45bcbb
Wiphawanyaun <gitwiphawan@gmail.com>
2023-08-03 16:22:03 +0700

    commit detaill

--- script.js
+++ script.js
@@ -18,7 +18,29 @@ const splide = new Splide('.splide',{
       pagination: false,
       // pauseOnHover: true,
       // wheel: true,
+
 });splide.mount();
+
+const splide1 = new Splide('#splide1',{
+      type: 'slide', //slide, loop, fade
+      // padding: { y: 10 }, //css values
+      gap: '4.5rem', //1 rem default
+      // rewind: true,
+      speed: 500, //transition in milliseconds
+      // fixedWidth: '10vw',
+      // // width: '100vw',
+      perPage: 1,
+      // start: 3,
+      autoWidth:true,
+      perMove: 1,
+      // autoplay: true,
+      // interval: 3000,
+      arrows: false,
+      // drag   : false,
+      // pagination: false,
+      // pauseOnHover: true,
+      // wheel: true,
+});splide1.mount();
 
 
 
